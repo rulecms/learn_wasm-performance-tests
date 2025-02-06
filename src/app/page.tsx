320c8dd69809326c615997f277f975bef1a36b73
dreamerkumar <connectwithvkumar@gmail.com>
2025-02-05 20:40:41 -0500

    remove logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,12 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
         <ol>
           <li>
             Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
         <div className={styles.ctas}>
           <a
             className={styles.primary}
