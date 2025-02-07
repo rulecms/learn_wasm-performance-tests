5db412b18db7a6eae1ab748e30aa9c17a7ee7d66
dreamerkumar <connectwithvkumar@gmail.com>
2025-02-06 20:03:33 -0500

    dependencies

---
+++ wasm/target/wasm32-unknown-unknown/release/deps/js_sys-178ae7358f197054.d
@@ -0,0 +1,7 @@
+/Users/vishalkumar/code/learn_wasm-performance-tests/wasm/target/wasm32-unknown-unknown/release/deps/libjs_sys-178ae7358f197054.rmeta: /Users/vishalkumar/.cargo/registry/src/index.crates.io-6f17d22bba15001f/js-sys-0.3.77/src/lib.rs
+
+/Users/vishalkumar/code/learn_wasm-performance-tests/wasm/target/wasm32-unknown-unknown/release/deps/libjs_sys-178ae7358f197054.rlib: /Users/vishalkumar/.cargo/registry/src/index.crates.io-6f17d22bba15001f/js-sys-0.3.77/src/lib.rs
+
+/Users/vishalkumar/code/learn_wasm-performance-tests/wasm/target/wasm32-unknown-unknown/release/deps/js_sys-178ae7358f197054.d: /Users/vishalkumar/.cargo/registry/src/index.crates.io-6f17d22bba15001f/js-sys-0.3.77/src/lib.rs
+
+/Users/vishalkumar/.cargo/registry/src/index.crates.io-6f17d22bba15001f/js-sys-0.3.77/src/lib.rs:
